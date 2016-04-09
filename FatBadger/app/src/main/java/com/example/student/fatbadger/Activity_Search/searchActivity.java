0550fb55d589adfec2e5a413e73a886b75527abf
Jan Reinacher <janreinacher@gmail.com>
2016-04-09 11:40:19 -0700

    added YELP API gradle

--- FatBadger/app/src/main/java/com/example/student/fatbadger/Activity_Search/searchActivity.java
+++ FatBadger/app/src/main/java/com/example/student/fatbadger/Activity_Search/searchActivity.java
@@ -9,12 +9,22 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.student.fatbadger.R;
+import com.yelp.clientlib.connection.YelpAPI;
+import com.yelp.clientlib.connection.YelpAPIFactory;
+import com.yelp.clientlib.entities.SearchResponse;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import retrofit.Call;
 
 
 /**
  * Created by The Janitor on 4/5/16.
  */
 public class searchActivity extends AppCompatActivity {
+    private YelpAPI yelpAPI;
+    private YelpAPIFactory apiFactory;
     private EditText searchText;
     private Button searchButton;
     private RecyclerView RestaurantRecyclerView;
@@ -24,10 +34,23 @@ public class searchActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        apiFactory = new YelpAPIFactory("b9XjaVT2lGL8Z2lh2qzvpg", "XLGu0hiT-F-Vtz2cUMXnxDzM1rw", "XqGF2hUZXeNbZn8Vcx1cmpHtAHoLDKB-", "hxtwtRh3uaQN9kFos-n7_x3W5m4");
+        yelpAPI = apiFactory.createAPI();
         searchText = (EditText)findViewById(R.id.searchText);
         searchButton= (Button)findViewById(R.id.searchButton);
         RestaurantRecyclerView = (RecyclerView)findViewById(R.id.RestaurantRecyclerView);
     }
 
+
+   // Map<String, String> params = new HashMap<>();
+
+// general params
+   // params.put("term", "food");
+    //params.put("limit", "3");
+
+// locale params
+   // params.put("lang", "fr");
+
+   // Call<SearchResponse> call = yelpAPI.search("San Francisco", params);
 
 }
