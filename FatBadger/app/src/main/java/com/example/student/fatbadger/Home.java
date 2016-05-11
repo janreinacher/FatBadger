fa9a5b672c07df2a3dc191753b71f1dabf390f36
Jack Meixensperger <jmeixensperger@gmail.com>
2016-05-10 18:07:45 -0700

    fixed badger background

--- FatBadger/app/src/main/java/com/example/student/fatbadger/Home.java
+++ FatBadger/app/src/main/java/com/example/student/fatbadger/Home.java
@@ -7,7 +7,9 @@ public class Home extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        super.onCreate(savedInstanceState);
     }
+
+
 }
